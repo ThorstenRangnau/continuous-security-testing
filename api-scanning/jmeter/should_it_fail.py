f0c0941f051a5486d48029be57b64c29dbeec685
Remco <r.m.van.buijtenen@student.rug.nl>
2020-03-15 18:32:21 +0100

    better reporting of failure

--- api-scanning/jmeter/should_it_fail.py
+++ api-scanning/jmeter/should_it_fail.py
@@ -3,4 +3,8 @@ import json
 with open('/jmeter/results/zap/json/LoginTesting.JSON') as f:
 	data = json.load(f)
 	if len(data['alerts']) != 0:
+		print("==========================")
+		print("\nTEST RESULTS\n")
+		print("Identified %d security vulnerabilities. See gitlab-ci artifacts for further details\n" % len(data['alerts']))
+		print("==========================")
 		exit(1)
