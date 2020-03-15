3f5621d694139cb102a0d0520b427e14989b8d79
Remco <r.m.van.buijtenen@student.rug.nl>
2020-03-15 14:24:14 +0100

    move & ignore results + fix jmeter running in docker-compose

--- api-scanning/jmeter/entrypoint.sh
+++ api-scanning/jmeter/entrypoint.sh
@@ -12,8 +12,8 @@ x=$(($freeMem/10*8))
 n=$(($freeMem/10*2))
 export JVM_ARGS="-Xmn${n}m -Xms${s}m -Xmx${x}m"
 
-ls /jmeter
-rm -rf /jmeter/tests/security/securityreport > /dev/null 2>&1
+rm -rf /jmeter/results/security/securityreport > /dev/null 2>&1
+rm /jmeter/results/security/jmeter.log
 
 echo "START Running Jmeter on `date`"
 echo "JVM_ARGS=${JVM_ARGS}"
@@ -23,6 +23,7 @@ echo "jmeter args=$@"
 jmeter $@
 echo "END Running Jmeter on `date`"
 
+tail -f /dev/null
 #     -n \
 #    -t "/tests/${TEST_DIR}/${TEST_PLAN}.jmx" \
 #    -l "/tests/${TEST_DIR}/${TEST_PLAN}.jtl"
