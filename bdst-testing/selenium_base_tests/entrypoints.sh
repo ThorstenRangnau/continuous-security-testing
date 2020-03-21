fdebf9c1c94a961225044943bb605e37f591467b
Thorsten Rangnau <rangnau@adference.com>
2020-03-21 12:55:01 +0100

    Add docker network in order to use zap with static ip address as proxy for selenmiunbase tests

--- bdst-testing/selenium_base_tests/entrypoints.sh
+++ bdst-testing/selenium_base_tests/entrypoints.sh
@@ -2,9 +2,9 @@
 set -e
 echo "***** SeleniumBase Docker Machine *****"
 echo "Checking for ZAP"
-./wait-for-it.sh zap:8090 -t 30
+./wait-for-it.sh 172.28.1.1:8090 -t 60
 echo "Checking for WebGoat"
-./wait-for-it.sh webgoat:8080 -t 60
+./wait-for-it.sh 172.28.1.2:8080 -t 60
 
 # we actually have to run two selenium tests first one to register a user
 
